52204d593cd0b832c11473d417b6dc525cb46316
Carlos Arranz Luque <7903808@alumnos.ufv.es>
2022-03-19 13:41:21 +0100

    API hola mundo

---
+++ src/main/java/com/proyectos2/Pastillero/MedicamentoController.java
@@ -0,0 +1,34 @@
+package com.proyectos2.Pastillero;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class MedicamentoController {
+
+    @Autowired
+    private MedicamentoRepository repository;
+
+    @GetMapping("/saludo")
+    public List<Medicamento> saludo(){
+
+        Medicamento medicamento = new Medicamento("Paracetamol",200);
+
+
+        repository.delete(medicamento);
+
+        List<Medicamento> medicamentos = repository.findAll();
+
+        
+
+        String hola = "Hola mundo";
+
+        return medicamentos;
+    }
+}
