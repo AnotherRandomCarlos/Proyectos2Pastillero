440658897947f82b3bc40f917e80f7fb51f4cda9
Carlos Arranz Luque <7903808@alumnos.ufv.es>
2022-05-06 16:43:44 +0200

    probando nuevo medicamento

--- src/main/java/com/proyectos2/Pastillero/UsuarioController.java
+++ src/main/java/com/proyectos2/Pastillero/UsuarioController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+import java.util.Optional;
 import java.util.PrimitiveIterator;
 
 @RestController
@@ -25,6 +26,8 @@ public class UsuarioController {
         List <pruebaid> lista = repository.findAll();
 
 
+
+
         return lista;
     }
 
@@ -32,11 +35,20 @@ public class UsuarioController {
     public void saludo2(){
 
         pruebaid usuarionuevo = new pruebaid("roberto",20);
-        
+
 
         repository.save(usuarionuevo);
 
 
         return;
+    }
+
+    @GetMapping("/saludo4")
+    public Optional<pruebaid> saludo4(){
+
+        Optional<pruebaid> usuario = repository.findById(2);
+
+
+        return usuario;
     }
 }
